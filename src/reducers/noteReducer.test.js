6e84be120d3ad7d1cd5fc491a35415a5a59b251e
unknown <asederado@gmail.com>
2023-03-31 14:09:57 +0200

    6c Many reducers, redux toolkit

--- src/reducers/noteReducer.test.js
+++ src/reducers/noteReducer.test.js
@@ -5,15 +5,11 @@ describe('noteReducer', () => {
   test('returns new state with action NEW_NOTE', () => {
     const state = []
     const action = {
-      type: 'NEW_NOTE',
-      payload: {
-        content: 'the app state is in redux store',
-        important: true,
-        id: 1
-      }
+      type: 'notes/createNote',
+      payload: 'the app state is in redux store',
     }
 
-    //deepFreeze(state)
+    deepFreeze(state)
     const newState = noteReducer(state, action)
 
     expect(newState).toHaveLength(1)
@@ -34,13 +30,11 @@ describe('noteReducer', () => {
       }]
   
     const action = {
-      type: 'TOGGLE_IMPORTANCE',
-      payload: {
-        id: 2
-      }
+      type: 'notes/toggleImportanceOf',
+      payload: 2
     }
   
-    //deepFreeze(state)
+    deepFreeze(state)
     const newState = noteReducer(state, action)
   
     expect(newState).toHaveLength(2)
